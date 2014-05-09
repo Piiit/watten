cb9d51a8ebd18c83dd96d33560cddae84d1ecf09
U-pitiz29a\Piiit <Piiit@pitiz29a.(none)>
2014-05-09 16:27:06 +0200

    watten test environment for tmux...

---
+++ Watten/starttest.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+BINDIR=~/eclipse-workspace/watten/Watten/bin/
+CLIENT=com/mpp/testing/ClientTest
+CLIENT2=com/mpp/network/ClientForConsole
+SERVER=com/mpp/network/Server
+
+SERVERCMD="echo SERVER; cd $BINDIR; java.exe $SERVER; read -p 'Done. Press any key to close...' -n1 -s"
+CLIENTCMD="echo CLIENT; cd $BINDIR; java.exe $CLIENT 2>/dev/null; read -p 'Done. Press any key to close...' -n1 -s"
+CLIENT2CMD="echo CLIENT2; cd $BINDIR; java.exe $CLIENT2 2>/dev/null; /bin/bash"
+
+tmux kill-session -t watten-test-session
+tmux new -d -s watten-test-session "$SERVERCMD"\; \
+          split-window -d "$CLIENT2CMD" \; \
+		  split-window -d "$CLIENTCMD" \; \
+		  split-window -h "$CLIENTCMD" \; \
+		  selectp -t 2 \; \
+		  split-window -h "$CLIENTCMD" \; \
+		  selectp -t 4 \; \
+                attach \;
+				
+echo Killing all internal java processes...
+kill $(ps aux | grep 'java' | awk '{print $1}')
+echo Done.
+exit 0
